dbdd1ebe10d4a88de94e533de1b13b9bdce053d8
Helen Ye <00yhelen@gmail.com>
2018-02-28 13:18:50 -0500

    Add connecting

---
+++ svg.js
@@ -0,0 +1,53 @@
+var pic = document.getElementById("vimage");
+var h = pic.getAttribute("height");
+var w = pic.getAttribute("width");
+var cX = -1;
+var cY = -1;
+
+var drawCircle = function(x, y) {
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", x);
+    circle.setAttribute("cy", y);
+    circle.setAttribute("r", h / 25);
+    circle.setAttribute("fill", "red");
+    circle.setAttribute("stroke", "black");
+
+    pic.appendChild(circle);
+}
+
+var connect = function(x1, y1, x2, y2) {
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", x1);
+    line.setAttribute("y1", y1);
+    line.setAttribute("x2", x2);
+    line.setAttribute("y2", y2);
+    line.setAttribute("stroke", "black");
+
+    pic.appendChild(line);
+}
+
+// Currently sets svg fill to green
+var change = function(e) {
+    e.preventDefault();
+    this.setAttribute("fill", "green");
+}
+
+var draw = function(e) {
+    e.preventDefault();
+    if (e.toElement == this) {
+        var x = e.offsetX;
+        var y = e.offsetY;
+        drawCircle(x, y);
+        if (cX < 0 || cY < 0) {
+            cX = e.offsetX;
+            cY = e.offsetY;
+        } else {
+            connect(cX, cY, x, y);
+            cX = x;
+            cY = y;
+        }
+    }
+}
+
+// pic.addEventListener("click", change);
+pic.addEventListener("click", draw);
